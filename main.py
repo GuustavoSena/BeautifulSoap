9c13fd6f9e797b77d51832ea9e5fb77237739745
Gustavo Sena <GamingUnzado12@gmail.com>
2023-09-14 22:18:22 -0300

    Exporting Data to a txt file

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
-website = 'https://subslikescript.com/movie/Titanic-46435'
-result = requests.get(website)
+WEBSITE = 'https://subslikescript.com/movie/Titanic-46435'
+result = requests.get(WEBSITE)
 content = result.text
 
 soup = BeautifulSoup(content, 'lxml')
@@ -10,3 +10,8 @@ soup = BeautifulSoup(content, 'lxml')
 box_article = soup.find('article', class_='main-article')
 
 movie_title = box_article.find('h1').get_text()
+
+transcription = box_article.find('div', class_='full-script').get_text(strip=True, separator=' ')
+
+with open(f'{movie_title}.txt', 'w', encoding="utf-8") as file:
+    file.write(transcription)
